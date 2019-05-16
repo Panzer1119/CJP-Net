36511ac6797ed6293dc8b07fb71fbb4cc17abfc8
Paul Hagedorn <panzer1119@gmail.com>
2019-05-16 21:34:11 +0200

    Created MySQLUtil.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/net/sql/MySQLUtil.java
@@ -0,0 +1,58 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.net.sql;
+
+import de.codemakers.base.logger.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLUtil {
+    
+    public static final String FORMAT_CONNECTION_H_D_U_P = "jdbc:mysql://%s/%s?user=%s&password=%s";
+    public static final String FORMAT_CONNECTION_H_D = "jdbc:mysql://%s/%s";
+    
+    static {
+        //testForDriver();
+    }
+    
+    @Deprecated
+    private static void testForDriver() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception ex) {
+            Logger.handleError(ex);
+        }
+    }
+    
+    public static Connection connect(String host, String database, String username, byte[] password) throws Exception {
+        return connect(host, database, username, new String(password));
+    }
+    
+    public static Connection connect(String host, String database, String username, String password) throws Exception {
+        return connect(String.format(FORMAT_CONNECTION_H_D_U_P, host, database, username, password));
+    }
+    
+    public static Connection connect(String connectionString) throws Exception {
+        return DriverManager.getConnection(connectionString);
+    }
+    
+    public static Connection connect(String connectionString, String username, String password) throws Exception {
+        return DriverManager.getConnection(connectionString, username, password);
+    }
+    
+}
